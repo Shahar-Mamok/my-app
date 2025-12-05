f7c4ca57c349445e67ba6a551f3c053e4d7dd661
Shahar=Mamok <Shaharmamok7@outlook.com>
2025-12-05 18:32:45 +0200

    feat: implement initial chat API with OpenAI integration and conversation state management

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,14 +1,10 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { OpenAI } from 'openai';
 import z from 'zod';
+import { chatService } from './services/chat.service';
 
 dotenv.config();
-
-const client = new OpenAI({
-   apiKey: process.env.OPENAI_API_KEY,
-});
 
 const app = express();
 app.use(express.json());
@@ -23,8 +19,6 @@ app.get('/api/hello', (req: Request, res: Response) => {
 });
 
 let lastResponseId: string | null = null;
-// conversationId -> lastResponseId
-const conversation = new Map<string, string>();
 
 const chatSchema = z.object({
    prompt: z
@@ -41,19 +35,15 @@ app.post('/api/chat', async (req: Request, res: Response) => {
       res.status(400).json(parseResult.error.format());
       return;
    }
-   const { prompt, conversationId } = req.body;
 
-   const response = await client.responses.create({
-      model: 'gpt-4o-mini',
-      input: prompt,
-      temperature: 0.2,
-      max_output_tokens: 100,
-      previous_response_id: conversation.get(conversationId),
-   });
+   try {
+      const { prompt, conversationId } = req.body;
+      const response = await chatService.sendMessage(prompt, conversationId);
 
-   conversation.set(conversationId, response.id);
-
-   res.json({ message: response.output_text });
+      res.json({ message: response.message });
+   } catch (error) {
+      res.status(500).json({ error: 'Failed to generate a response.' });
+   }
 });
 
 app.listen(PORT, () => {
---
+++ packages/server/repositories/conversation.repository.ts
@@ -0,0 +1,11 @@
+// Implemation detail
+const conversation = new Map<string, string>();
+
+export const conversationRepository = {
+   getLastResponseId(conversationId: string) {
+      return conversation.get(conversationId);
+   },
+   setLastResponseId(conversationId: string, responseId: string) {
+      return conversation.set(conversationId, responseId);
+   },
+};

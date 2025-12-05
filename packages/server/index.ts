0cfb5df1ea42776ab5b20a9c5e91c9e725881e72
Shahar=Mamok <Shaharmamok7@outlook.com>
2025-12-05 13:44:31 +0200

    feat: scaffold client and server applications with basic API and OpenAI integration

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { OpenAI } from 'openai';
+import z from 'zod';
+
 dotenv.config();
 
 const client = new OpenAI({
@@ -20,15 +22,37 @@ app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello World!' });
 });
 
+let lastResponseId: string | null = null;
+// conversationId -> lastResponseId
+const conversation = new Map<string, string>();
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'prompt is required.')
+      .max(1000, 'prompt is too long (max 1000 characters)'),
+   conversationId: z.string().uuid(),
+});
+
 app.post('/api/chat', async (req: Request, res: Response) => {
-   const { prompt } = req.body;
+   const parseResult = chatSchema.safeParse(req.body);
+   if (!parseResult.success) {
+      res.status(400).json(parseResult.error.format());
+      return;
+   }
+   const { prompt, conversationId } = req.body;
 
    const response = await client.responses.create({
       model: 'gpt-4o-mini',
       input: prompt,
       temperature: 0.2,
       max_output_tokens: 100,
+      previous_response_id: conversation.get(conversationId),
    });
+
+   conversation.set(conversationId, response.id);
+
    res.json({ message: response.output_text });
 });
 

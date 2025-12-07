490651f900cbf2634ff1d3ea588229b7044856a0
Shahar=Mamok <Shaharmamok7@outlook.com>
2025-12-07 16:22:31 +0200

    feat: add new client package with ChatBot UI for API interaction.

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,16 +1,23 @@
 import { Button } from './ui/button';
 import { FaArrowUp } from 'react-icons/fa';
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { useRef } from 'react';
 
 type FormData = {
    prompt: string;
 };
 
 const ChatBot = () => {
+   const conversationId = useRef(crypto.randomUUID());
    const { register, handleSubmit, reset, formState } = useForm<FormData>();
-   const onSubmit = (data: FormData) => {
-      console.log(data);
+   const onSubmit = async ({ prompt }: FormData) => {
       reset();
+      const { data } = await axios.post('/api/chat', {
+         prompt,
+         conversationId: conversationId.current,
+      });
+      console.log(data);
    };
    const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
       if (e.key === 'Enter' && !e.shiftKey) {

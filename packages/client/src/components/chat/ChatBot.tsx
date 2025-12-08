ad363eefcc7a2f2c52eaf4301bea6629ec06991a
Shahar=Mamok <Shaharmamok7@outlook.com>
2025-12-08 19:43:22 +0200

    Extracting chatmessages component

--- packages/client/src/components/chat/ChatBot.tsx
+++ packages/client/src/components/chat/ChatBot.tsx
@@ -5,6 +5,8 @@ import { Button } from '../ui/button';
 import { FaArrowUp } from 'react-icons/fa';
 import { useForm } from 'react-hook-form';
 import TypingIndicator from './TypingIndicator';
+import type { Message } from './ChatMessages';
+import ChatMessages from './ChatMessages';
 
 type FormData = {
    prompt: string;
@@ -12,22 +14,13 @@ type FormData = {
 type ChatResponse = {
    message: string;
 };
-type Message = {
-   role: 'user' | 'bot';
-   content: string;
-};
 
 const ChatBot = () => {
    const [messages, setMessages] = useState<Message[]>([]);
    const [isBotTyping, setIsBotTyping] = useState(false);
    const [error, setError] = useState('');
-   const lastMessageRef = useRef<HTMLDivElement | null>(null);
    const conversationId = useRef(crypto.randomUUID());
    const { register, handleSubmit, reset, formState } = useForm<FormData>();
-
-   useEffect(() => {
-      lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
-   }, [messages]);
 
    const onSubmit = async ({ prompt }: FormData) => {
       try {
@@ -57,30 +50,11 @@ const ChatBot = () => {
          handleSubmit(onSubmit)();
       }
    };
-   const onCopyMessage = (e: React.ClipboardEvent) => {
-      const selection = window.getSelection()?.toString().trim();
-      if (selection) {
-         e.preventDefault();
-         e.clipboardData?.setData('text/plain', selection);
-      }
-   };
+
    return (
       <div className="flex flex-col h-full">
          <div className="flex flex-col flex-1 gap-3 mb-10 overflow-y-auto">
-            {messages.map((message, index) => (
-               <div
-                  key={index}
-                  onCopy={onCopyMessage}
-                  ref={index === messages.length - 1 ? lastMessageRef : null}
-                  className={`px-3 py-1 rounded-xl ${
-                     message.role === 'user'
-                        ? 'bg-blue-600 text-white self-end'
-                        : 'bg-gray-100 text-black self-start'
-                  }`}
-               >
-                  <ReactMarkdown>{message.content}</ReactMarkdown>
-               </div>
-            ))}
+            <ChatMessages messages={messages} />
             {isBotTyping && <TypingIndicator />}
             {error && <p className="text-red-500">{error}</p>}
          </div>

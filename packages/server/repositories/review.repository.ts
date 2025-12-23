233592075661b298e3aeac6cefe6b03c9b8b7506
Shahar=Mamok <Shaharmamok7@outlook.com>
2025-12-23 21:03:29 +0200

    Storing the summary

--- packages/server/repositories/review.repository.ts
+++ packages/server/repositories/review.repository.ts
@@ -1,13 +1,28 @@
 import { PrismaClient, type Review } from '../generated/prisma';
-
+import dayjs from 'dayjs';
+const prisma = new PrismaClient();
 export const reviewRepository = {
    async getReviews(productId: number, limit?: number): Promise<Review[]> {
-      const prisma = new PrismaClient();
-
       return prisma.review.findMany({
          where: { productId: productId },
          orderBy: { createdAt: 'desc' },
          take: limit,
+      });
+   },
+   storeReviewSummary(productId: number, summary: string) {
+      const now = new Date();
+      const expiresAt = dayjs().add(7, 'day').toDate();
+      const data = {
+         expiresAt,
+         generatedAt: now,
+         productId,
+         content: summary,
+      };
+
+      return prisma.summary.upsert({
+         where: { productId },
+         create: data,
+         update: data,
       });
    },
 };
--- packages/server/services/review.service.ts
+++ packages/server/services/review.service.ts
@@ -1,7 +1,7 @@
 import { type Review } from '../generated/prisma';
 import { reviewRepository } from '../repositories/review.repository';
 import { llmClient } from '../llm/client';
-
+import template from '../prompts/summarize-reviews.txt';
 export const reviewService = {
    async getReviews(productId: number): Promise<Review[]> {
       return reviewRepository.getReviews(productId);
@@ -10,16 +10,14 @@ export const reviewService = {
       //get the last 10 reviews
       const reviews = await reviewRepository.getReviews(productId, 10);
       const joinedReviews = reviews.map((r) => r.content).join('\n\n');
-      const prompt = `
-      Summarize the following customer reviews into a short paragraph
-      highlighting key themes, both positive and negative:
-      ${joinedReviews}`;
-      const response = await llmClient.generateText({
+      const prompt = template.replace('{{reviews}}', joinedReviews);
+      const { text: summary } = await llmClient.generateText({
          model: 'gpt-4o-mini',
          prompt,
          temperature: 0.2,
          max_tokens: 500,
       });
-      return response.text;
+      await reviewRepository.storeReviewSummary(productId, summary);
+      return summary;
    },
 };

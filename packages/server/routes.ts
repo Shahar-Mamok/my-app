6e2471892f3cf5091db1b99799308b4f26daa190
Shahar=Mamok <Shaharmamok7@outlook.com>
2025-12-23 19:57:22 +0200

    Refactoring separation of concerns

--- packages/server/routes.ts
+++ packages/server/routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import { chatController } from './controllers/chat.controller';
 import { PrismaClient } from './generated/prisma';
+import { reviewController } from './controllers/reviews.controller';
 
 const router = express.Router();
 router.get('/', (req: Request, res: Response) => {
@@ -14,20 +15,6 @@ router.get('/api/hello', (req: Request, res: Response) => {
 
 router.post('/api/chat', chatController.sendMessage);
 
-router.get('/api/products/:id/reviews', async (req: Request, res: Response) => {
-   const prisma = new PrismaClient();
-   const productId = Number(req.params.id);
-
-   if (isNaN(productId)) {
-      return res.status(400).json({ error: 'Invalid product ID' });
-   }
-
-   const reviews = await prisma.review.findMany({
-      where: { productId: productId },
-      orderBy: { createdAt: 'desc' },
-   });
-
-   res.json(reviews);
-});
+router.get('/api/products/:id/reviews', reviewController.getReviews);
 
 export default router;
